b3cb8cab507c55983e5be8f68559d5085eab9945
Scott Gose <scott.gose@elastic.co>
2025-11-05 06:28:13 -0600

    Improve utility scripts

--- bin/change-solution-view.sh
+++ bin/change-solution-view.sh
@@ -1,10 +1,44 @@
+#!/usr/bin/env bash
+
+# Prompt the user to pick a Kibana solution view
+echo "Select the Kibana solution view:"
+echo "1) Elasticsearch"
+echo "2) Observability"
+echo "3) Security"
+echo "4) Classic"
+read -rp "Enter the number of your choice [1-4]: " choice
+
+# Map the user's choice to the corresponding solution key
+case $choice in
+1)
+  solution="es"
+  ;;
+2)
+  solution="oblt"
+  ;;
+3)
+  solution="security"
+  ;;
+4)
+  solution="classic"
+  ;;
+*)
+  echo "Invalid choice. Defaulting to 'es' (Elasticsearch)."
+  solution="es"
+  ;;
+esac
+
+# Display the chosen solution
+echo "Setting Kibana 'default' space to solution view: $solution"
+
+# Send the update request to Kibana
 curl -X PUT "http://localhost:5601/api/spaces/space/default" \
   -H "kbn-xsrf: true" \
   -H "Content-Type: application/json" \
   -u elastic:elastic \
-  -d '{
-    "id": "default",
-    "name": "Default",
-    "disabledFeatures": [],
-    "solution": "es"
-  }'
+  -d "{
+    \"id\": \"default\",
+    \"name\": \"Default\",
+    \"disabledFeatures\": [],
+    \"solution\": \"$solution\"
+  }"
--- bin/toggle-dark-mode.sh
+++ bin/toggle-dark-mode.sh
@@ -1,11 +1,28 @@
-#!/bin/bash
-# Enable Dark mode
-curl -X PUT "http://localhost:5601/api/saved_objects/config/9.2.0" \
+#!/usr/bin/env bash
+set -euo pipefail
+
+KIBANA_URL="${KIBANA_URL:-http://localhost:5601}"
+VERSION="${KIBANA_VERSION:-9.2.0}"
+AUTH="${KIBANA_AUTH:-elastic:elastic}"
+
+# Read current value (default to "false" if missing/null)
+CURRENT=$(
+  curl -s -u "$AUTH" -H "kbn-xsrf: true" \
+    "$KIBANA_URL/api/saved_objects/config/$VERSION" |
+    jq -r '.attributes["theme:darkMode"] // "false"'
+)
+
+# Toggle
+if [[ "$CURRENT" == "true" ]]; then NEW=false; else NEW=true; fi
+
+# Write new value (no fragile quotes thanks to a here-doc)
+curl -s -X PUT "$KIBANA_URL/api/saved_objects/config/$VERSION" \
   -H "kbn-xsrf: true" \
   -H "Content-Type: application/json" \
-  -u elastic:elastic \
-  -d '{
-    "attributes": {
-      "theme:darkMode": true
-    }
-  }'
+  -u "$AUTH" \
+  --data-binary @- <<JSON
+{"attributes":{"theme:darkMode":$NEW}}
+JSON
+
+echo
+echo "Dark mode toggled to: $NEW"
